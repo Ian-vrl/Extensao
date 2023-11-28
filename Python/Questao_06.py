69092725a8e0ee707353170f7813bedcf895c630
Ian-vrl <ianvictorlima09@gmail.com>
2023-11-28 02:17:07 -0300

    adicionando questao 07

--- Python/Questao_06.py
+++ Python/Questao_06.py
@@ -1,1 +1,14 @@
 # Verificação de Números Pares e Múltiplos de 3
+numero = int()
+
+print("Digite um número inteiro:")
+numero = int(input())
+
+if numero % 2 == 0:
+
+    if numero % 3 == 0:
+        print("O número é par e múltiplo de 3.")
+    else:
+        print("O número é par, mas não é múltiplo de 3.")
+else:
+    print("O número não é par.")
